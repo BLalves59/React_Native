283bc0b8b4ab579c77dfe95a9e17c4fd315f0602
BLalves59 <167263927+BLalves59@users.noreply.github.com>
2024-05-22 20:39:51 -0300

    aula 22.05

---
+++ src/ex_8/addItem/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    botao:{
+
+    }
+  },
+)
+  
+export default styles;
--- src/ex_8/index.js
+++ src/ex_8/index.js
@@ -2,24 +2,40 @@ import { useState } from 'react';
 import { View, FlatList, } from 'react-native';
 
 import Header from './header';
-import ItemLista from './itemlista';
+import ItemLista from './itemLista/index';
 
 import styles from './styles';
 
 export default function Exemplo8 () {
     const [itens, setItens] = useState([
-        {id: 1, item: 'leite'},
-        {id: 2, item: 'maça'},
-        {id: 3, item: 'alfaçe'},
-        {id: 4, item: 'mamão'},
-        {id: 4, item: 'banana'}
+        {id: 1, nome: 'leite'},
+        {id: 2, nome: 'maça'},
+        {id: 3, nome: 'alfaçe'},
+        {id: 4, nome: 'mamão'},
+        {id: 5, nome: 'banana'}
     ]);
+
+    function deleteItem (id){
+        setItens(
+            prevItens => {
+            return prevItens.filter(
+                (item) => item.id != id
+                );
+            }
+        );
+    }
+
     return (
         <View style={styles.container}>
             <Header />
             <FlatList
                 data={itens}
-                renderItem={(objeto) => <ItemLista objeto={objeto.item}/> }
+                renderItem={(objeto) => 
+                <ItemLista
+                    dados={objeto.item}
+                    deleteItem={deleteItem}    
+                /> }
+                keyExtractor={item => item.id}
             />
         </View>
     );
